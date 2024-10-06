97846ad61db57feaa734a1c98e72da8cb1e64833
Pablos <pablo.salcedom@gmail.com>
2024-10-05 20:40:58 -0400

    adding sections

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -15,7 +15,33 @@ const EventPlanner = () => {
                 </p>
             </section>
             <section className="event_categories">
-
+                <ul>
+                    <h2>Social Events:</h2>
+                    <li>Birthday parties</li>
+                    <li>Anniversary celebrations</li>
+                    <li>Wedding receptions</li>
+                    <li>Baby showers</li>
+                    <li>Graduation parties</li>
+                    <li>Family reunions</li>
+                </ul>
+                <ul>
+                    <h2>Community Events:</h2>
+                    <li>Fundraising events</li>
+                    <li>Charity galas</li>
+                    <li>Volunteer drives</li>
+                    <li>Neighborhood block parties</li>
+                    <li>Community festivals</li>
+                    <li>Cultural celebrations</li>
+                </ul>
+                <ul>
+                    <h2>Entertainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music festivals</li>
+                    <li>Film screenings</li>
+                    <li>Comedy shows</li>
+                    <li>Art exhibitions</li>
+                    <li>Cultural events</li>
+                </ul>
             </section>
             <section className="features">
                 
